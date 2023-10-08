dc317e1e609b2f2cd862b801deb13adabe4fda88
matador7495 <id4acc2014@gmail.com>
2023-10-09 00:41:01 +0330

    responsive

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const header = document.querySelector("header");
-
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 0);
+});
+const menu = document.querySelector("#menu-icon");
+const navlist = document.querySelector(".navlist");
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("active");
+};
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("active");
+};
+const sr = ScrollReveal({
+  distance: "35px",
+  duration: 2700,
+  reset: true,
+});
+sr.reveal(".home-text", { delay: 350, origin: "left" });
+sr.reveal(".home-img", { delay: 350, origin: "right" });
+sr.reveal(".sub-service,.about,.contact,.portfolio,.service,.cta", {
+  delay: 20,
+  origin: "bottom",
 });
