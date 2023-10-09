7e5c0ea9bddffb8b1235ca8211ad493de2abcfbb
matador7495 <id4acc2014@gmail.com>
2023-10-09 13:32:43 +0330

    fix bug

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const sr = ScrollReveal({
   duration: 2700,
   reset: true,
 });
-sr.reveal(".home-text", { delay: 350, origin: "left" });
-sr.reveal(".home-img", { delay: 350, origin: "right" });
+sr.reveal(".home-text", { delay: 350, origin: "top" });
+sr.reveal(".home-img", { delay: 350, origin: "top" });
 sr.reveal(".sub-service,.about,.contact,.portfolio,.service,.cta", {
   delay: 20,
   origin: "bottom",
